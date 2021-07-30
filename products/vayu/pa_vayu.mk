a564d1ec224ab47276eed4e9d218fccf477931df
Foxtrot47 <jjneutron@outlook.com>
2021-07-30 13:51:07 +0000

    pa: Add vayu target
    Change-Id: I851e12a77251030c4b6b5189d08c415a6b5f90a7

---
+++ products/vayu/pa_vayu.mk
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2021 Project Fluid
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common PA stuff.
+$(call inherit-product, vendor/pa/config/common_full_phone.mk)
+
+# Inherit from vayu device
+$(call inherit-product, device/xiaomi/vayu/device.mk)
+
+PRODUCT_NAME := pa_vayu
+PRODUCT_DEVICE := vayu
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := POCO X3 Pro
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="coral-user 11 RQ3A.210705.001 7380771 release-keys"
+
+BUILD_FINGERPRINT :=  google/coral/coral:11/RQ3A.210705.001/7380771:user/release-keys
+
+TARGET_BOOT_ANIMATION_RES := 1080
